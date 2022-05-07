dddab675fa7866a7428efda19ef0f739a64ff17e
はりけん <92016933+hnm876-md@users.noreply.github.com>
2022-05-07 19:12:01 +0900

    削ぎ落とし
    余計なもんとった。それだけ

--- odoshi.js
+++ odoshi.js
@@ -2,19 +2,6 @@ import $ from "https://cdn.skypack.dev/jquery@3.6.0";
 import * as platform from "https://cdn.skypack.dev/platform@1.3.6";
 
 
-
-$("#date").text(new Date().toLocaleDateString());
-
-$(function(){
-$('#demo1').text(new Date());
-});
-
-$(function(){
-setInterval(function(){
-$('#demo2').text(new Date());
-}, 1000);
-});
-
 $(function(){
 var now = new Date();
 var y = now.getFullYear();
@@ -27,15 +14,3 @@ var mi = now.getMinutes();
 var s = now.getSeconds();
 $('#demo3').text('登録日時：'+ y + '年' + m + '月' + d + '日' + h + '時' + mi + '分' + s + '秒' + '(' + wd[w] + ')');
 });
-
-$("device").text(platform.description);
-
-
-
-
-let c = 21600;
-
-setInterval(() => {
-  $("sec").text(c);
-  c = c - 1;
-},1000)
